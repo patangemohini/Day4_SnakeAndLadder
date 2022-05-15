70075c1eba44f2bb55920e121954327071d7b50b
mohini patange <patangemohini5@gmail.com>
2022-05-16 01:47:47 +0530

    Added : rolling dice

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -1,8 +1,10 @@
 package snakeandladder;
 
 public class SnakeAndLadder {
-    public static void main(String[] args){
+    public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder program!!");
         int playerPosition = 0;
+        int number = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("Number after rolling the dice by the player : " + number);
     }
 }
