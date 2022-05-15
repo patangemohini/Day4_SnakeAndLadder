4eb9421cf0bc9f8efa407cdcf0ce5bb40d203000
mohini patange <patangemohini5@gmail.com>
2022-05-16 01:07:31 +0530

    Added : player start position

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -3,5 +3,6 @@ package snakeandladder;
 public class SnakeAndLadder {
     public static void main(String[] args){
         System.out.println("Welcome to snake and ladder program!!");
+        int playerPosition = 0;
     }
 }
