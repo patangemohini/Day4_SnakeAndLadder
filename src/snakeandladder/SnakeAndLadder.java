f1f0bde28187f998e5f20bafa1237912e18cd673
mohini patange <patangemohini5@gmail.com>
2022-05-17 00:05:49 +0530

    Added : Repeating till player reach win position 100

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -4,30 +4,37 @@ public class SnakeAndLadder {
 
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder program!!");
+
         int playerPosition = 0;
-        int dieNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println("DieNumber :" + dieNumber);
-        int choice = (int) Math.floor(Math.random() * 10) % 3;
-        switch (choice) {
-            case 0:
-                System.out.println("No play : Player stays in the same position");
-                playerPosition = playerPosition;
-                System.out.println("Player position : " + playerPosition);
-                break;
-            case 1:
-                System.out.println("Ladder : player moves ahead by the position");
-                playerPosition = +dieNumber;
-                System.out.println("Player position : " + playerPosition);
-                break;
-            case 2:
-                System.out.println("Snake : player moves behind by the position");
-                if (dieNumber > playerPosition) {
+        int startPosition = 0;
+        int endPosition = 100;
+
+        while (playerPosition < endPosition) {
+            int dieNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+            System.out.println("DieNumber :" + dieNumber);
+            int choice = (int) Math.floor(Math.random() * 10) % 3;
+
+            switch (choice) {
+                case 0:
+                    System.out.println("No play : Player stays in the same position");
                     playerPosition = playerPosition;
-                } else {
-                    playerPosition = -dieNumber;
-                }
-                System.out.println("Player position : " + playerPosition);
-                break;
+                    System.out.println("Player position : " + playerPosition);
+                    break;
+                case 1:
+                    System.out.println("Ladder : player moves ahead by the position");
+                    playerPosition += dieNumber;
+                    System.out.println("Player position : " + playerPosition);
+                    break;
+                case 2:
+                    System.out.println("Snake : player moves behind by the position");
+                    if (dieNumber > playerPosition) {
+                        playerPosition = startPosition;
+                    } else {
+                        playerPosition -= dieNumber;
+                    }
+                    System.out.println("Player position : " + playerPosition);
+                    break;
+            }
         }
     }
 }
