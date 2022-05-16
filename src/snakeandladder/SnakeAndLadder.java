a17092a5ca9bcd9f73ce25c269d1c4dbdd637b57
mohini patange <patangemohini5@gmail.com>
2022-05-16 23:41:38 +0530

    Added : Player position options no play, ladder & snake

--- src/snakeandladder/SnakeAndLadder.java
+++ src/snakeandladder/SnakeAndLadder.java
@@ -1,10 +1,33 @@
 package snakeandladder;
 
 public class SnakeAndLadder {
+
     public static void main(String[] args) {
         System.out.println("Welcome to snake and ladder program!!");
         int playerPosition = 0;
-        int number = (int) Math.floor(Math.random() * 10) % 6 + 1;
-        System.out.println("Number after rolling the dice by the player : " + number);
+        int dieNumber = (int) Math.floor(Math.random() * 10) % 6 + 1;
+        System.out.println("DieNumber :" + dieNumber);
+        int choice = (int) Math.floor(Math.random() * 10) % 3;
+        switch (choice) {
+            case 0:
+                System.out.println("No play : Player stays in the same position");
+                playerPosition = playerPosition;
+                System.out.println("Player position : " + playerPosition);
+                break;
+            case 1:
+                System.out.println("Ladder : player moves ahead by the position");
+                playerPosition = +dieNumber;
+                System.out.println("Player position : " + playerPosition);
+                break;
+            case 2:
+                System.out.println("Snake : player moves behind by the position");
+                if (dieNumber > playerPosition) {
+                    playerPosition = playerPosition;
+                } else {
+                    playerPosition = -dieNumber;
+                }
+                System.out.println("Player position : " + playerPosition);
+                break;
+        }
     }
 }
